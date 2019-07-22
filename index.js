98a1aeacebf9f257ab2ea02463086cac9b486d66
Matthew Chiam <machiam@microsoft.com>
2019-07-22 15:21:44 -0700

    Draw present grids on a canvas

--- index.js
+++ index.js
@@ -8,18 +8,58 @@ for (var i = 1; i <= 31; i++) {
     dateNumbers.push("" + i)
 }
 var dates = words.filter(function (word) { return word.text in dateNumbers })
-var sortedDates = dates.sort(function (a, b) { return parseInt(a.text) - parseInt(b.text) })
+var sortedDates = getNonDuplicateDates(dates.sort(function (a, b) { return parseInt(a.text) - parseInt(b.text) }))
+
 console.log("Recognized dates", sortedDates)
 
 // Try and get grid dimensions
 var width = getGridWidth(sortedDates)
 console.log("Width", width)
-
 var height = getGridHeight(sortedDates)
 console.log("Height", height)
 
+// Get missing dates
+var presentDates = sortedDates.map(function (date) { return date.text })
+var missingDates = dateNumbers.filter(function(candidate) { return !presentDates.includes(candidate) })
+console.log("Missing dates", missingDates)
 
+// Given existing dates, convert them to grid dimensions
+var presentGrids = sortedDates.map(function (date) {
+    var topRightX = date.boundingBox[2]
+    var topRightY = date.boundingBox[3]
+    return {
+        text: date.text,
+        topLeft: [topRightX - width, topRightY],
+        topRight: [topRightX, topRightY],
+        bottomRight: [topRightX, topRightY + height],
+        bottomLeft: [topRightX - width, topRightY + height]
+    }
+})
+console.log("Present grids", presentGrids)
 
+// Draw existing grids
+var canvas = document.getElementById("canvas")
+var context = canvas.getContext("2d")
+for (var i = 0; i < presentGrids.length; i++) {
+    var grid = presentGrids[i]
+    context.rect(grid.topLeft[0], grid.topLeft[1], width, height)
+    context.stroke()
+
+    context.font = "100px Arial"
+    context.strokeText(grid.text, grid.topLeft[0], grid.topLeft[1])
+}
+
+function getNonDuplicateDates(sortedDatesInner) {
+    var singles = []
+    var dateTextOnly = sortedDatesInner.map(function (date) { return date.text })
+    var grouped = dateTextOnly.reduce((a, c) => (a[c] = (a[c] || 0) + 1, a), Object.create(null))
+    for (var date in grouped) {
+        if (grouped[date] === 1) {
+            singles.push(date)
+        }
+    }
+    return sortedDatesInner.filter(function (date) { return singles.includes(date.text) })
+}
 
 function getGridWidth(sortedDatesInner) {
     var result = []
